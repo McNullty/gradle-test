696c78ba337425090c4214e79077c79a2671fa78
Mladen Čikara <mladen.cikara@gmail.com>
2016-01-30 15:59:11 +0100

    Initisl import.

---
+++ src/test/java/hr/css/helloworld/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package hr.css.helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
